ee6391ee0ea44242303114e8a3f3112e0bb0b12b
GitHub Action <action@github.com>
2022-03-14 01:00:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/heavenosk/classnamesfold/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/heavenosk/classnamesfold/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.heavenosk.classnamesfold.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.heavenosk.classnamesfold.MyBundle
 
 class MyApplicationService {
 
